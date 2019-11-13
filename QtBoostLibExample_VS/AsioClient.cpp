a539da2b9f517d1f90984cc2fa2615db0a4838e5
taehyeong77@naver.com <taehyeong77@naver.com>
2019-11-13 22:26:05 +0900

    cc

--- QtBoostLibExample_VS/AsioClient.cpp
+++ QtBoostLibExample_VS/AsioClient.cpp
@@ -2,11 +2,18 @@
 
 
 
-AsioClient::AsioClient()
+AsioClient::AsioClient() :
+	work(new boost::asio::io_service::work(ioservice))
 {
+	worker = std::thread([&]() {
+		ioservice.run();
+	});
 }
 
 
 AsioClient::~AsioClient()
 {
+	ioservice.stop();
+	worker.join();		// thread의 종료를 기다린다.
+	work.reset();		// shared_ptr 메모리 해제
 }
--- QtBoostLibExample_VS/AsioClient.h
+++ QtBoostLibExample_VS/AsioClient.h
@@ -3,11 +3,21 @@
 #include "boost/asio.hpp"
 
 #include <QObject>
+#include <qstring.h>
+#include <memory>
+#include <thread>
 
 class AsioClient
 {
 public:
 	AsioClient();
 	~AsioClient();
+
+	void Get(const QString& url, const QString& path);
+
+private:
+	boost::asio::io_service ioservice;
+	std::shared_ptr<boost::asio::io_service::work> work;
+	std::thread worker;
 };
 
